1d79e89875766d8280a9e5d597ea81a1ef2fbf17
unknown <gonzalocarrascosec@gmail.com>
2020-10-14 16:01:24 -0500

    ISSUE#4 fix de BaseControllerImpl

--- src/main/java/com/example/alumno/controllers/BaseControllerImpl.java
+++ src/main/java/com/example/alumno/controllers/BaseControllerImpl.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-public abstract class BaseControllerImpl<E extends Base, S extends BaseServiceImpl<E, Long>> implements com.example.alumno.controllers.BaseController<E, Long> {
+public abstract class BaseControllerImpl<E extends Base, S extends BaseServiceImpl<E, Long>> implements BaseController<E, Long> {
     @Autowired
     protected S servicio;
 
